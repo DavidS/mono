bb571cba0268d4640597d0aa602a4ecd1663219d
David Schmitt <david@dasz.at>
2011-04-12 18:08:51 +0200

    Add NUnit 2.6 snapshot from http://lists.ximian.com/pipermail/mono-devel-list/2011-April/037349.html

---
+++ nunit26/NUnitFramework/framework/Interfaces/ITestAction.cs
@@ -0,0 +1,19 @@
+﻿// ****************************************************************
+// Copyright 2011, Charlie Poole
+// This is free software licensed under the NUnit license. You may
+// obtain a copy of the license at http://nunit.org
+// ****************************************************************
+
+#if CLR_2_0 || CLR_4_0
+using System;
+
+namespace NUnit.Framework
+{
+    /// <summary>
+    /// The base interface for test actions
+    /// </summary>
+    public interface ITestAction
+    {
+    }
+}
+#endif
---
+++ nunit26/NUnitFramework/framework/Interfaces/ITestSuiteAction.cs
@@ -0,0 +1,30 @@
+﻿// ****************************************************************
+// Copyright 2011, Charlie Poole
+// This is free software licensed under the NUnit license. You may
+// obtain a copy of the license at http://nunit.org
+// ****************************************************************
+
+#if CLR_2_0 || CLR_4_0
+using System;
+
+namespace NUnit.Framework
+{
+    /// <summary>
+    /// The interface implemented to provide actions to execute before and after suites.
+    /// </summary>
+    public interface ITestSuiteAction : ITestAction
+    {
+        /// <summary>
+        /// Executed before each suite is run
+        /// </summary>
+        /// <param name="fixture">The fixture that defines the test suite</param>
+        void BeforeTestSuite(object fixture);
+
+        /// <summary>
+        /// Executed after each suite is run
+        /// </summary>
+        /// <param name="fixture">The fixture that defines the test suite</param>
+        void AfterTestSuite(object fixture);
+    }
+}
+#endif
